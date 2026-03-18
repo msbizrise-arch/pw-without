f57eed5c87f3a1c341fb6b4cf9b4902049c10629
msbizrise-arch <msbizrise@gmail.com>
2026-03-18 14:15:15 +0530

    Update and rename mainMS.py to main.py

--- main.py
+++ main.py
@@ -48,9 +48,9 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 
 # Bot credentials from environment variables (Render compatible)
-API_ID = int(os.environ.get("API_ID", 24473318))
-API_HASH = os.environ.get("API_HASH", "e7dd0576c5ac0ff8f90971d6bb04c8f5")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0")
+API_ID = int(os.environ.get("API_ID", 38498066))
+API_HASH = os.environ.get("API_HASH", "c9696114751feacdeb1b4487f5839a1a")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8277629583:AAHFrekPPUxgY65KjbbNS5wVULsVeXrgPkg")
 
 # Initialize Bot Globally (IMPORTANT FIX)
 bot = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
